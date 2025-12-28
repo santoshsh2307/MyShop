1b07b4556c62ff976cedc6c6dced87eda95d44e7
Santosh <78397565+santoshsh2307@users.noreply.github.com>
2025-12-28 11:24:13 +0530

    remove spacec

--- MyShop/src/test/java/com/myshop/demo/MyShopApplicationTests.java
+++ MyShop/src/test/java/com/myshop/demo/MyShopApplicationTests.java
@@ -2,8 +2,6 @@ package com.myshop.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-
 // testing added btn
 @SpringBootTest
 class MyShopApplicationTests {
